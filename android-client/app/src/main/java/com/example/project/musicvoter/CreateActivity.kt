96487cf481261c4a3d7b5c86f828f3f7f6b4e3f7
LubomirHristov <lubobo99@mail.bg>
2019-07-28 17:31:41 +0100

    POST requests for room creation working.

--- android-client/app/src/main/java/com/example/project/musicvoter/CreateActivity.kt
+++ android-client/app/src/main/java/com/example/project/musicvoter/CreateActivity.kt
@@ -3,7 +3,16 @@ package com.example.project.musicvoter
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import android.widget.EditText
 import kotlinx.android.synthetic.main.activity_create.*
+import java.io.BufferedReader
+import java.io.DataOutputStream
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import java.net.HttpURLConnection
+import java.net.URL
+import java.nio.charset.StandardCharsets
+import java.util.*
 
 
 class CreateActivity : AppCompatActivity() {
@@ -19,5 +28,54 @@ class CreateActivity : AppCompatActivity() {
         val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, items)
         //set the spinners adapter to the previously created one.
         spinner1.adapter = adapter
+
+        createRoomBtn.setOnClickListener{
+            val name = roomName as EditText
+            val pass = roomPassword as EditText
+
+
+
+            val message = "name=${name.text}&owner_uid=${UUID.randomUUID()}&password=${pass.text}"
+
+            val url = URL("http://4562829a.ngrok.io/api/rooms")
+
+            val connection = url.openConnection() as HttpURLConnection
+            connection.requestMethod = "POST"
+
+
+            val postData: ByteArray = message.toByteArray(StandardCharsets.UTF_8)
+
+            connection.setRequestProperty("charset", "utf-8")
+            connection.setRequestProperty("Content-lenght", postData.size.toString())
+
+            Thread(Runnable {
+                try {
+                    val outputStream: DataOutputStream = DataOutputStream(connection.outputStream)
+                    outputStream.write(postData)
+                    outputStream.flush()
+                } catch (exception: Exception) {
+                    exception.printStackTrace()
+                }
+
+                if (connection.responseCode != HttpURLConnection.HTTP_OK) {
+                    try {
+
+
+                        val reader: BufferedReader = BufferedReader(InputStreamReader(connection.inputStream))
+                        val output: String = reader.readLine()
+
+                        println("There was error while connecting the chat $output")
+                        System.exit(0)
+
+                    } catch (exception: Exception) {
+                        throw Exception("Exception while push the notification  $exception.message")
+                    }
+                }
+
+            }).start()
+
+
+
+        }
     }
 }
