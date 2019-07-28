53a060e7d46957a476f70a680bd21bf0cc239fa9
Metodi <metodi.istatkov@gmail.com>
2019-07-28 15:57:28 +0100

    JoinActivity outlined

--- android-client/app/src/main/java/com/example/project/musicvoter/JoinActivity.kt
+++ android-client/app/src/main/java/com/example/project/musicvoter/JoinActivity.kt
@@ -2,11 +2,18 @@ package com.example.project.musicvoter
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.activity_join.*
 
 class JoinActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_join)
+
+        val rooms = arrayListOf<String>("Room1", "Room2", "Room3")
+        var dataAdapter = ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, rooms)
+        spinnerJoin.adapter = dataAdapter
     }
+
 }
