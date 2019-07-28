16859659a6c2a6e621f27a479afaac3daa6fc333
LubomirHristov <lubobo99@mail.bg>
2019-07-28 15:45:32 +0100

    Activities displayed

--- android-client/app/src/main/java/com/example/project/musicvoter/CreateActivity.kt
+++ android-client/app/src/main/java/com/example/project/musicvoter/CreateActivity.kt
@@ -3,7 +3,6 @@ package com.example.project.musicvoter
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
-import android.widget.Spinner
 import kotlinx.android.synthetic.main.activity_create.*
 
 
@@ -14,11 +13,11 @@ class CreateActivity : AppCompatActivity() {
         setContentView(R.layout.activity_create)
 
         //create a list of items for the spinner.
-        val items = arrayOf("1", "2", "three")
+        val items = arrayOf("1", "2", "3", "4", "5", "6", "7", "8", "9", "10")
         //create an adapter to describe how the items are displayed, adapters are used in several places in android.
         //There are multiple variations of this, but this is the basic variant.
         val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, items)
         //set the spinners adapter to the previously created one.
-        spinner1.setAdapter(adapter)
+        spinner1.adapter = adapter
     }
 }
